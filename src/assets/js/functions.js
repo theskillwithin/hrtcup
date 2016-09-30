237ed4aa3438c84fdc8ddc49d10dc0b6b3efd26e
theskillwithin <austinppeterson@gmail.com>
2016-09-30 14:59:57 -0700

    starting responsive header

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
-
+	
+	var mq = window.matchMedia( "(max-width: 890px)" ); // NAV
+	mq.addListener(function(){
+	if (mq.matches) {
+		console.log('test');
+	}
+});
 });
