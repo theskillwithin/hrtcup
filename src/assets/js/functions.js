709bb6d8fd5295ad4205f546e6f383658001a38b
theskillwithin <austinppeterson@gmail.com>
2016-09-30 22:21:35 -0700

    update js

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -1,43 +1,44 @@
-$(document).ready(function () {
+$(function () {
     // owl carousels
-  if (!jQuery.fn.andSelf) { jQuery.fn.andSelf = jQuery.fn.addBack }  // https://github.com/OwlCarousel2/OwlCarousel2/issues/1441
+  if (!jQuery.fn.andSelf) {
+    jQuery.fn.andSelf = jQuery.fn.addBack
+  } // https://github.com/OwlCarousel2/OwlCarousel2/issues/1441
+
   $('#trendingProjects').find('.owl-carousel').owlCarousel({
     itemElement: 'article',
     stageElement: 'div',
     items: 3
   })
 
-  var mq = window.matchMedia('(max-width: 890px)'),
-    $header = $('header'),
-    $headerNav = $header.find('nav')
+  var mq = window.matchMedia('(max-width: 890px)')
+  var header = $('header')
+  var headerNav = header.find('nav')
 
   if (mq.matches) {
-    $headerNav.find('ul').addClass('mobile')
-    $headerNav.find('.user').addClass('mobile')
+    headerNav.find('ul, .user').addClass('mobile')
 
-    $('html').on('click', function (e) {
-      if (!$('header nav ul').hasClass('mobile')) {
-        $('header .hamburger').trigger('click')
+    $(document).on('click', function (e) {
+      if (!headerNav.find('ul').hasClass('mobile')) {
+        header.find('.hamburger').trigger('click')
       }
     })
-    $('header .top').click(function () {
+
+    header.find('.top').click(function () {
       return false
     })
   }
 
   mq.addListener(function () {
     if (mq.matches) {
-      $('header nav ul').addClass('mobile')
-      $('header nav .user').addClass('mobile')
-      $('header .hamburger').addClass('mobile')
-
-            // close on click outside header
+      headerNav.find('ul, .user')
+                .add(header.find('.hamburger'))
+                    .addClass('mobile')
     }
   })
 
-  $('header .hamburger').on('click', function () {
-    $('header nav ul').toggleClass('mobile')
-    $('header nav .user').toggleClass('mobile')
-    $('header .hamburger').toggleClass('mobile')
+  header.find('.hamburger').on('click', function () {
+    headerNav.find('ul, .user')
+            .add(header.find('.hamburger'))
+                .toggleClass('mobile')
   })
 })
