00b9864edbf1e41d89b83d75670b5fa1e38f6967
Austin Peterson <austinair@Austins-MacBook-Air.local>
2016-10-30 04:33:28 -0600

    update js es6

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -1,60 +1,58 @@
-  'use strict'
+const hrtcup = {
+  runSwiper() {
+    const mySwiper = new Swiper('.swiper-container', {
+      loop: true,
+      slideClass: 'swiper-slide',
+      spaceBetween: 10,
+      centeredSlides: true,
+      pagination: '.swiper-pagination',
+      slidesPerView: 'auto',
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev'
+    });
+  },
+  mobileNav() {
+    const mq = window.matchMedia('(max-width: 890px)');
+    const header = document.querySelector('header');
+    const headerNav = header.querySelector('nav');
+    const navUl = headerNav.querySelector('ul');
+    const navUser = headerNav.querySelector('.user');
+    const navHamburger = header.querySelector('.hamburger');
+    const top = document.querySelector('header .top');
+    if (mq.matches) {
+      headerFire()
+    }
 
-  var hrtcup = {
-    runSwiper: function () {
-      var mySwiper = new Swiper('.swiper-container', {
-        loop: true,
-        slideClass: 'swiper-slide',
-        spaceBetween: 10,
-        centeredSlides: true,
-        pagination: '.swiper-pagination',
-        slidesPerView: 'auto',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev'
-      })
-    },
-    mobileNav: function () {
-      var mq = window.matchMedia('(max-width: 890px)'),
-        header = document.querySelector('header'),
-        headerNav = header.querySelector('nav'),
-        navUl = headerNav.querySelector('ul'),
-        navUser = headerNav.querySelector('.user'),
-        navHamburger = header.querySelector('.hamburger'),
-        top = document.querySelector('header .top')
+    mq.addListener(() => {
       if (mq.matches) {
         headerFire()
       }
+    })
 
-      mq.addListener(function () {
-        if (mq.matches) {
-          headerFire()
+    function headerFire () {
+      navUl.classList.add('mobile')
+      navUser.classList.add('mobile')
+
+      document.addEventListener('click', event => {
+        if (event.target === top) return
+        if (event.target === top.querySelector('.container')) return
+
+        if (!navUl.classList.contains('mobile')) {
+          navUl.classList.toggle('mobile')
+          navUser.classList.toggle('mobile')
+          navHamburger.classList.toggle('mobile')
         }
       })
-
-      function headerFire () {
-        navUl.classList.add('mobile')
-        navUser.classList.add('mobile')
-
-        document.addEventListener('click', function (event) {
-          if (event.target === top) return
-          if (event.target === top.querySelector('.container')) return
-
-          if (!navUl.classList.contains('mobile')) {
-            navUl.classList.toggle('mobile')
-            navUser.classList.toggle('mobile')
-            navHamburger.classList.toggle('mobile')
-          }
-        })
-      }
-
-      navHamburger.addEventListener('click', function (event) {
-        navUl.classList.toggle('mobile')
-        navUser.classList.toggle('mobile')
-        navHamburger.classList.toggle('mobile')
-        event.stopPropagation()
-      })
     }
+
+    navHamburger.addEventListener('click', event => {
+      navUl.classList.toggle('mobile')
+      navUser.classList.toggle('mobile')
+      navHamburger.classList.toggle('mobile')
+      event.stopPropagation()
+    })
   }
-  hrtcup.runSwiper()
-  hrtcup.mobileNav()
+};
+hrtcup.runSwiper()
+hrtcup.mobileNav()
 
